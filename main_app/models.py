b4e561598863a26b845d09248c9cf11499276226
royal0817 <martinezl27737@gmail.com>
2022-06-02 16:47:58 -0400

    cleaned up a bit

--- main_app/models.py
+++ main_app/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-# Create your models here.
-
-
-# LOW = 1
-# MID = 2
-# HIGH = 3
-# CHOICES = (
-#     (LOW, 'Low')
-#     (MID, 'Medium')
-#     (HIGH, 'High')
-# )
-
-
 class Champion(models.Model):
     name = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100)
